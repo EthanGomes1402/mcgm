e3dfab761d49a3e010c28aba87e45a8ba51485f0
dhirajshinde121083 <dhiraj@aarohan.biz>
2022-04-23 13:27:27 +0530

    added some changes to seggrate offline online vehicles

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 import json,re,string,random,inspect
 from common.models import Ward
-from swmadmin.models import Vehicle
+from swmadmin.models import Vehicle,Installation
 from swmadmin.models import Ewd
 from reports.models import Current_tracklog_history
+from reports.models import Vehicle_tracklog_history
 from django.db.models.aggregates import Max
 from django.contrib.gis.geos import GEOSGeometry,Point,LineString
 from django.contrib.auth.decorators import login_required,user_passes_test
@@ -126,15 +127,19 @@ def get_quick_view_form_param(request):
 @user_passes_test(lambda user: user.is_superuser or (user.appuser.is_contractor or user.appuser.is_officer))
 def latest_vehicle_status_v2(request):
     response_data=dict()
-    response_data['status'] = 'success'
-    response_data['data'] = list()
-    scvcount = 0
-    lccount = 0
-    mccount = 0
-    wwcount = 0
-    dwcount = 0
-    swcount = 0
-    othercount = 0
+    response_data['status']         = 'success'
+    response_data['data']           = dict()
+    response_data['data']['online'] = list()
+    response_data['data']['offline']= list()
+    installations                   = dict()
+    scvcount                        = 0
+    lccount                         = 0
+    mccount                         = 0
+    wwcount                         = 0
+    dwcount                         = 0
+    swcount                         = 0
+    othercount                      = 0
+    totalcount                      = 0
 
 
     #ajit's code block 1 starts - initializing the dictionary from txt file.
@@ -153,8 +158,9 @@ def latest_vehicle_status_v2(request):
         else:
             iward = request.user.appuser.bmc_officer.ward
 
-    for each_th_record_dict in Current_tracklog_history.objects.values('vehicle').annotate(max_id=Max('id')).order_by('max_id'):
-        each_th_record = Current_tracklog_history.objects.get(id=each_th_record_dict['max_id'])
+    for each_th_record_dict in Current_tracklog_history.objects.values('vehicle').annotate(max_dt=Max('datetime')).order_by('max_dt'):
+        each_th_record = Current_tracklog_history.objects.get(datetime=each_th_record_dict['max_dt'],vehicle_id=each_th_record_dict['vehicle'])
+        totalcount+=1
         each_vehicle_record_data = dict()
         each_vehicle_record_data['veh'] = str(each_th_record.vehicle)
         each_vehicle_record_data['veh_ward'] = str(each_th_record.vehicle.ward)
@@ -192,12 +198,14 @@ def latest_vehicle_status_v2(request):
             resultaddress = "Out of Mumbai."
         #each_vehicle_record_data['address'] = resultaddress
         each_vehicle_record_data['ward'] = resultaddress.split(",")[0] if resultaddress.split(",")[0] else ' '
+
         #ajit code block 2 ends here
 
         #try:
             #each_vehicle_record_data['ward'] = Ewd.objects.filter(is_active=True).filter(ewd_fence__contains=each_vehicle_record_data['location']).get()
         #except:
             #each_vehicle_record_data['ward'] = None
+
         tm = each_th_record.datetime + timedelta(minutes=0)
         each_vehicle_record_data['time'] = str(tm.strftime("%Y-%m-%d %H:%M:%S"))
         #logic will check if current time is in assigned route schedule for a vehicle
@@ -217,37 +225,15 @@ def latest_vehicle_status_v2(request):
         else:
             othercount+=1
 
-        """ each_vehicle_record_data['trip_status'] = 'In trip'
-        each_vehicle_record_data['speed'] = each_th_record.speed
-        if each_vehicle_record_data['speed']:
-            if each_vehicle_record_data['speed'] > 60:
-                each_vehicle_record_data['speed_status']='Above Alarming'
-                each_vehicle_record_data['speed_class'] ='red'
-                each_vehicle_record_data['veh_status'] ='Running'
-                each_vehicle_record_data['veh_class'] ='green'
-            elif each_vehicle_record_data['speed'] > 40 and each_vehicle_record_data['speed'] <= 60:
-                each_vehicle_record_data['speed_status']='Alarming'
-                each_vehicle_record_data['speed_class'] ='green'
-                each_vehicle_record_data['veh_status'] ='Running'
-                each_vehicle_record_data['veh_class'] ='green'
-            else:
-                each_vehicle_record_data['speed_status']='Normal'
-                each_vehicle_record_data['speed_class'] ='orange'
-                each_vehicle_record_data['veh_status'] ='Running'
-                each_vehicle_record_data['veh_class'] ='green'
+        if tm.date() < datetime.today().date():
+           response_data['data']['offline'].append(each_vehicle_record_data)
         else:
-            each_vehicle_record_data['speed_status']='No Speed'
-            each_vehicle_record_data['speed_class'] ='aqua'
-            each_vehicle_record_data['veh_status'] ='Idle'
-            each_vehicle_record_data['veh_class'] ='orange'
+           response_data['data']['online'].append(each_vehicle_record_data)
 
-        each_vehicle_record_data['mps'] = str(each_th_record.mps)
-        each_vehicle_record_data['miv'] = str(each_th_record.miv)
-        each_vehicle_record_data['ibv'] = str(each_th_record.ibv)
-        #each_vehicle_record_data['location'] = str(each_th_record.location)
-        each_vehicle_record_data['ignition_status'] = str(each_th_record.ignition)
-        each_vehicle_record_data['emergency_status'] = str(each_th_record.emergency)
-        each_vehicle_record_data['digital_io_status'] = str(each_th_record.dio) """
-        response_data['data'].append(each_vehicle_record_data)
+    #all_vth_record = Vehicle_tracklog_history.objects.filter(vehicle_id__in=installations.keys()).values('vehicle').annotate(max_dt=Max('datetime')).order_by('max_dt') 
+        #for each_th_record in all_vth_record:
+        #    each_vehicle_record_data = dict()
+
+    #print(all_vth_record)
     #vehicles  = list(map(lambda vehicle : Vehicle.objects.get(pk=vehicle) , form_data['selectVehicle'].split("_")))
-    return render(request,'dashboard/latest_vehicle_status.html',{ 'all_entries' : response_data['data'], 'scvcount' : scvcount, 'lccount' : lccount, 'mccount' : mccount, 'swcount' : swcount, 'dwcount' : dwcount, 'wwcount': wwcount, 'othercount' : othercount})
+    return render(request,'dashboard/latest_vehicle_status.html',{ 'all_online_entries' : response_data['data']['online'],'all_offline_entries' : response_data['data']['offline'] , 'scvcount' : scvcount, 'lccount' : lccount, 'mccount' : mccount, 'swcount' : swcount, 'dwcount' : dwcount, 'wwcount': wwcount, 'othercount' : othercount,'totalvehiclecount' : totalcount })
